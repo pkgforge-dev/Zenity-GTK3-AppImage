82ee3ff87ba0983926693d95d79918d06335f4bd
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2024-04-22 14:53:05 -0400

    Update zenity-gtk3-appimage.sh

--- zenity-gtk3-appimage.sh
+++ zenity-gtk3-appimage.sh
@@ -1,28 +1,30 @@
 #!/bin/sh
 
-APP=zenity-gtk3
+APP=Zenity-gtk3
+APPDIR=Zenity.AppDir
 SITE="https://github.com/Samueru-sama/zenity.git"
 
 # CREATE DIRECTORIES
 if [ -z "$APP" ]; then exit 1; fi
-mkdir -p ./"$APP" ./"$APP/$APP".AppDir/usr/bin ./"$APP/$APP".AppDir/usr/share/zenity && cd ./"$APP" || exit 1
+mkdir -p ./"$APP/$APPDIR" && cd ./"$APP/$APPDIR" || exit 1
 
-# DOWNLOAD AND BUILD ZENITY
-git clone "$SITE" && cd ./zenity && meson --prefix /usr . build && meson compile -C build || exit 1
-
-# PREPARE APPIMAGE FILES
-cd .. && mv ./zenity/build/src/zenity ./"$APP".AppDir/usr/bin && mv ./zenity/src/zenity.ui ./"$APP".AppDir/usr/share/zenity && mv ./zenity/data/* ./"$APP".AppDir/usr/share/zenity || exit 1
+# DOWNLOAD AND MAKE ZENITY
+CURRENTDIR="$(readlink -f "$(dirname "$0")")" # DO NOT MOVE THIS
+git clone "$SITE" && cd ./zenity && meson --prefix "$CURRENTDIR" . build && meson compile -C build && meson install -C build # NORMAL ERROR HERE, HAVEN'T FIXED
+cd .. && find ./bin/* -type f -executable -exec sed -i -e "s|/usr|././|g" {} \; && echo "binary patched" && rm -rf ./zenity # Patch binary
 
 # AppRun
-cd "./$APP.AppDir" || exit 1
 cat >> ./AppRun << 'EOF'
 #!/bin/sh
 CURRENTDIR="$(readlink -f "$(dirname "$0")")"
-export XDG_DATA_DIRS="$CURRENTDIR/usr/share/:$XDG_DATA_DIRS"
-export ZENITY_DATA_DIR="$CURRENTDIR/usr/share/zenity"
-exec "$CURRENTDIR/usr/bin/zenity" "$@"
+export PATH="$CURRENTDIR/bin:$PATH"
+export XDG_DATA_DIRS="$CURRENTDIR/share:$XDG_DATA_DIRS"
+export ZENITY_DATA_DIR="$CURRENTDIR/share/zenity"
+exec "$CURRENTDIR/bin/zenity" "$@"
 EOF
 chmod a+x ./AppRun
+APPVERSION=$(./AppRun --version)
+if [ -z "$APPVERSION" ]; then echo "Failed to get version from zenity"; exit 1; fi
 
 # DESKTOP & ICON
 #DESKTOP="https://gitlab.gnome.org/GNOME/zenity/-/raw/master/data/org.gnome.Zenity.desktop.in?ref_type=heads"  #wget "$DESKTOP" -O ./$APP.desktop # appimagetool complains with this .desktop file
@@ -39,7 +41,7 @@ StartupNotify=true
 Categories=Utility;
 X-GNOME-UsesNotifications=true
 EOF
-cp ./usr/share/zenity/zenity.png ./ && ln -s ./zenity.png ./.DirIcon || exit 1
+cp ./share/zenity/zenity.png ./ && ln -s ./zenity.png ./.DirIcon || exit 1
 
 # MAKE APPIMAGE
 cd ..
@@ -47,12 +49,8 @@ APPIMAGETOOL=$(wget -q https://api.github.com/repos/probonopd/go-appimage/releas
 wget -q "$APPIMAGETOOL" -O ./appimagetool && chmod a+x ./appimagetool
 
 # Do the thing!
-ARCH=x86_64 VERSION=$(./appimagetool -v | grep -o '[[:digit:]]*') ./appimagetool -s ./$APP.AppDir
+ARCH=x86_64 VERSION="$APPVERSION" ./appimagetool -s ./"$APPDIR"
 ls ./*.AppImage || { echo "appimagetool failed to make the appimage"; exit 1; }
-
-APPNAME=$(ls *AppImage)
-APPVERSION=3.44
-mv ./*AppImage ./"$APPVERSION"-"$APPNAME"
 if [ -z "$APP" ]; then exit 1; fi # Being extra safe lol
 mv ./*.AppImage .. && cd .. && rm -rf "./$APP"
 echo "All Done!"
