0e32c260c18dcf69f0b0057f518ee9d600ceeaa9
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2024-05-19 02:34:52 -0400

    Update zenity-gtk3-appimage.sh

--- zenity-gtk3-appimage.sh
+++ zenity-gtk3-appimage.sh
@@ -1,62 +1,60 @@
 #!/bin/sh
 
-APP=clipman
-APPDIR="$APP".AppDir
+APP=Zenity-gtk3
+APPDIR=Zenity.AppDir
+SITE="https://github.com/Samueru-sama/zenity.git"
 
 # CREATE DIRECTORIES
 if [ -z "$APP" ]; then exit 1; fi
-mkdir -p ./"$APP" && cd ./"$APP" || exit 1
+mkdir -p ./"$APP/$APPDIR" && cd ./"$APP/$APPDIR" || exit 1
 
-# DOWNLOAD AND BUILD CLIPMAN STATICALLY
+# DOWNLOAD AND MAKE ZENITY
 CURRENTDIR="$(readlink -f "$(dirname "$0")")" # DO NOT MOVE THIS
-CFLAGS='-static -O3'
-LDFLAGS='-static'
-git clone --recursive https://github.com/Samueru-sama/xfce4-clipman-plugin.git \
-&& cd ./xfce4-clipman* && ./autogen.sh && make && make install DESTDIR="$CURRENTDIR" || exit 1
-cd .. && rm -rf ./xfce4-clipman-plugin && mv ./usr/local ./"$APPDIR" && rmdir ./usr || exit 1
-
-# PREPARE APPIMAGE
-cd ./"$APPDIR" && cp ./share/applications/xfce4-clipman.desktop ./xfce4-clipman-plugin.desktop \
-&& cp ./share/icons/hicolor/scalable/apps/*svg ./xfce4-clipman-plugin.svg \
-&& ln -s ./*.svg ./.DirIcon || exit 1
-
-# THIS IS A HACK
-LIBSPATH=$(ldd ./bin/xfce4-clipman | awk '{print $3}')
-mkdir ./lib 
-cp $LIBSPATH ./lib
-rm -f ./lib/libc.so* ./lib/libcrypto.so* ./lib/libstdc++.so* ./lib/libgnutls.so* ./lib/ld-linux* \
-./lib/libgio* ./lib/libglib* ./lib/libX11* ./lib/libmvec* ./lib/libm.* ./lib/libsl.* ./lib/libresolv* \
-./lib/libnss* ./lib/libc.so* ./lib/libm.so* ./lib/libBroken* ./lib/libc_malloc*
+CFLAGS="-static -O3" 
+LDFLAGS="-static"
+git clone "$SITE" && cd ./zenity && meson setup build -Dprefix="$CURRENTDIR" -Ddefault_library=static \
+&& ninja -C build && ninja -C build install # NORMAL ERROR HERE, HAVEN'T FIXED
+cd .. && find ./bin/* -type f -executable -exec sed -i -e "s|/usr|././|g" {} \; && echo "binary patched" && rm -rf ./zenity # Patch binary
 
 # AppRun
 cat >> ./AppRun << 'EOF'
-#!/bin/bash
-CURRENTDIR="$(dirname "$(readlink -f "$0")")"
-export PATH="$PATH:$CURRENTDIR/bin"
-export LD_LIBRARY_PATH="/lib:/lib64:/usr/lib:/usr/lib64:$CURRENTDIR/lib:$LD_LIBRARY_PATH"
-
-if [ "$1" = "history" ]; then
-	"$CURRENTDIR"/bin/xfce4-clipman-history "${@:2}"
-elif [ "$1" = "settings" ]; then
-	"$CURRENTDIR"/bin/xfce4-clipman-settings "${@:2}"
-elif [ "$1" = "popup" ]; then
-	"$CURRENTDIR"/bin/xfce4-popup-clipman "${@:2}"
-elif [ "$1" = "start" ]; then
-	 "$CURRENTDIR"/bin/xfce4-clipman "${@:2}"
-else
-	echo "Options are: 'history' 'settings' 'popup' and 'start'"
-	echo "Use 'start' to start clipman daemon in the backgroud"
-fi
+#!/bin/sh
+CURRENTDIR="$(readlink -f "$(dirname "$0")")"
+export PATH="$CURRENTDIR/bin:$PATH"
+export XDG_DATA_DIRS="$CURRENTDIR/share:$XDG_DATA_DIRS"
+export ZENITY_DATA_DIR="$CURRENTDIR/share/zenity"
+exec "$CURRENTDIR/bin/zenity" "$@"
 EOF
 chmod a+x ./AppRun
-APPVERSION=notray
+#APPVERSION=$(./AppRun --version) # This fails here because zenity needs a display to give you the version lol
+APPVERSION=3.44
+if [ -z "$APPVERSION" ]; then echo "Failed to get version from zenity"; exit 1; fi
 
-# Do the thing!
+# DESKTOP & ICON
+#DESKTOP="https://gitlab.gnome.org/GNOME/zenity/-/raw/master/data/org.gnome.Zenity.desktop.in?ref_type=heads"  #wget "$DESKTOP" -O ./$APP.desktop # appimagetool complains with this .desktop file
+cat >> ./$APP.desktop << 'EOF'
+[Desktop Entry]
+Name=Zenity
+Comment=Display dialog boxes from the command line
+Exec=zenity
+Icon=zenity
+Terminal=false
+Type=Application
+NoDisplay=true
+StartupNotify=true
+Categories=Utility;
+X-GNOME-UsesNotifications=true
+EOF
+cp ./share/zenity/zenity.png ./ && ln -s ./zenity.png ./.DirIcon || exit 1
+
+# MAKE APPIMAGE
 cd ..
 APPIMAGETOOL=$(wget -q https://api.github.com/repos/probonopd/go-appimage/releases -O - | sed 's/"/ /g; s/ /\n/g' | grep -o 'https.*continuous.*tool.*86_64.*mage$')
 wget -q "$APPIMAGETOOL" -O ./appimagetool && chmod a+x ./appimagetool
+
+# Do the thing!
 ARCH=x86_64 VERSION="$APPVERSION" ./appimagetool -s ./"$APPDIR"
 ls ./*.AppImage || { echo "appimagetool failed to make the appimage"; exit 1; }
 if [ -z "$APP" ]; then exit 1; fi # Being extra safe lol
-mv ./*.AppImage .. && cd .. && rm -rf ./"$APP"
+mv ./*.AppImage .. && cd .. && rm -rf "./$APP"
 echo "All Done!"
