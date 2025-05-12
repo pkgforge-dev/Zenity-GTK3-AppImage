d16b6eaeb9d2aa647d2c109c39d941bc64f25f79
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-05-12 07:39:15 -0400

    revert commit 0f29fb4

--- zenity-gtk3-appimage.sh
+++ zenity-gtk3-appimage.sh
@@ -40,7 +40,7 @@ xvfb-run -a -- ./lib4bin -p -v -s -k -e \
 	/usr/lib/gdk-pixbuf-*/*/*/* \
 	/usr/lib/gio/modules/libgvfsdbus*
 
-ln -s ./bin/zenity ./AppRun
+ln ./sharun ./AppRun
 ./sharun -g
 
 echo '[Desktop Entry]
