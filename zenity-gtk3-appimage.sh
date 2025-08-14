9f82261f6457faddc6ed60af0ba3a602165d02b2
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-08-14 02:16:43 -0400

    deploy with anylinux wrapper scripts

--- zenity-gtk3-appimage.sh
+++ zenity-gtk3-appimage.sh
@@ -1,74 +1,43 @@
 #!/bin/sh
 
-set -eu
+set -eux
 
-export ARCH="$(uname -m)"
-export APPIMAGE_EXTRACT_AND_RUN=1
+ARCH="$(uname -m)"
 
-REPO="https://gitlab.gnome.org/GNOME/zenity.git"
-APPIMAGETOOL="https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-$ARCH.AppImage"
-UPINFO="gh-releases-zsync|$(echo $GITHUB_REPOSITORY | tr '/' '|')|latest|*$ARCH.AppImage.zsync"
-LIB4BIN="https://raw.githubusercontent.com/VHSgunzo/sharun/refs/heads/main/lib4bin"
-
-# Prepare AppDir
-mkdir -p ./AppDir
-cd ./AppDir
-
-git clone "$REPO" ./zenity && (
+# Build zenity
+git clone "https://gitlab.gnome.org/GNOME/zenity.git" ./zenity && (
 	cd ./zenity
 	git checkout "zenity-3-44"
-	meson setup build  --prefix=/usr
+	meson setup build --prefix=/usr
 	meson compile -C build
-	DESTDIR=../../ meson install --no-rebuild -C build
+	DESTDIR=/usr meson install --no-rebuild -C build
 )
 
-mv ./usr/share ./
-mv ./usr ./shared
-rm -rf ./zenity ./share/help
+# Prepare AppDir
+VERSION="$(xvfb-run -a -- "$(command -v zenity)" --version)"
+[ -n "$VERSION" ] && echo "$VERSION" > ~/version
 
-# zenity is hardcoded to look for files in /usr/share
-# we will fix it with binary patching
-sed -i 's|/usr/share|././/share|g' ./shared/bin/zenity
-echo 'SHARUN_WORKING_DIR=${SHARUN_DIR}' > ./.env
+URUNTIME="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/uruntime2appimage.sh"
+SHARUN="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/quick-sharun.sh"
 
-# bundle dependencies
-wget "$LIB4BIN" -O ./lib4bin
-chmod +x ./lib4bin
-xvfb-run -a -- ./lib4bin -p -v -s -k -e \
-	./shared/bin/zenity -- --question --text "English or Spanish?"
-./lib4bin -p -v -s -k \
-	/usr/lib/gdk-pixbuf-*/*/*/* \
-	/usr/lib/gio/modules/libgvfsdbus*
+export OUTNAME=zenity-"$VERSION"-anylinux-"$ARCH".AppImage
+export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
+export DESKTOP=DUMMY
+export ICONs=DUMMY
+export PATH_MAPPING_RELATIVE=1 # zenity is hardcoded to look for files in /usr/share
 
-ln ./sharun ./AppRun
-./sharun -g
+# MAKE APPIAMGE WITH URUNTIME
+wget --retry-connrefused --tries=30 "$SHARUN" -O ./quick-sharun
+chmod +x ./quick-sharun
+./quick-sharun /usr/bin/zenity -- --question --text "English or Spanish?"
 
-echo '[Desktop Entry]
-Name=Zenity
-Comment=Display dialog boxes from the command line
-Exec=zenity
-Terminal=false
-Type=Application
-NoDisplay=true
-StartupNotify=true
-Categories=Utility
-Icon=zenity' > ./zenity.desktop
-touch ./zenity.png
 
-export VERSION="$(xvfb-run -a -- ./AppRun --version)"
-echo "$VERSION" > ~/version
-
-# MAKE APPIAMGE WITH FUSE3 COMPATIBLE APPIMAGETOOL
-cd ..
-wget "$APPIMAGETOOL" -O ./appimagetool
-chmod +x ./appimagetool
-./appimagetool -n -u "$UPINFO" \
-	"$PWD"/AppDir "$PWD"/zenity-"$VERSION"-anylinux-"$ARCH".AppImage
-
-wget -qO ./pelf "https://github.com/xplshn/pelf/releases/latest/download/pelf_$ARCH"
+# make appbundle
+wget --retry-connrefused --tries=30 \
+	"https://github.com/xplshn/pelf/releases/latest/download/pelf_$ARCH" -O ./pelf
 chmod +x ./pelf
 echo "Generating [dwfs]AppBundle..."
-UPINFO="gh-releases-zsync|$(echo $GITHUB_REPOSITORY | tr '/' '|')|latest|*$ARCH.dwfs.AppBundle.zsync"
+UPINFO="$(echo "$UPINFO" | sed 's#.AppImage.zsync#*.AppBundle.zsync#g')"
 ./pelf --add-appdir ./AppDir \
 	--appimage-compat                         \
 	--add-updinfo "$UPINFO"                   \
