88a4ab5756b00aeeaa07ffe8ea24267d644bc236
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-08-27 22:54:40 +0000

    move to archlinux (#12)

--- zenity-gtk3-appimage.sh
+++ zenity-gtk3-appimage.sh
@@ -4,19 +4,7 @@ set -eux
 
 ARCH="$(uname -m)"
 
-# Build zenity
-git clone "https://gitlab.gnome.org/GNOME/zenity.git" ./zenity && (
-	cd ./zenity
-	git checkout "zenity-3-44"
-	meson setup build --prefix=/usr
-	meson compile -C build
-	meson install --no-rebuild -C build
-)
-
-# Prepare AppDir
-VERSION="$(awk -F":|'" '/version:/{print $3; exit}' ./zenity/meson.build)"
-[ -n "$VERSION" ] && echo "$VERSION" > ~/version
-
+VERSION="$(cat ~/version)"
 URUNTIME="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/uruntime2appimage.sh"
 SHARUN="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/quick-sharun.sh"
 
@@ -24,7 +12,7 @@ export OUTNAME=zenity-"$VERSION"-anylinux-"$ARCH".AppImage
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export DESKTOP=DUMMY
 export ICONs=DUMMY
-export PATH_MAPPING_RELATIVE=1 # zenity is hardcoded to look for files in /usr/share
+export URUNTIME_PRELOAD=1
 
 wget --retry-connrefused --tries=30 "$SHARUN" -O ./quick-sharun
 chmod +x ./quick-sharun
@@ -34,7 +22,6 @@ chmod +x ./quick-sharun
 wget --retry-connrefused --tries=30 "$URUNTIME" -O ./uruntime2appimage
 chmod +x ./uruntime2appimage
 ./uruntime2appimage
-
 
 # make appbundle
 wget --retry-connrefused --tries=30 \
