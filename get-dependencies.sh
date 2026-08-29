6dbb2b899d99b1782f843d0b4ac8a96ad3a8d1b4
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-08-29 18:30:43 -0400

    `--buildtype=release`

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -21,7 +21,7 @@ get-debloated-pkgs gtk3-mini libxml2-mini gdk-pixbuf2-mini librsvg-mini
 git clone "https://gitlab.gnome.org/GNOME/zenity.git" ./zenity 
 cd ./zenity
 git checkout "zenity-3-44"
-meson setup build --prefix=/usr
+meson setup build --buildtype=release --prefix=/usr
 meson compile -C build
 meson install --no-rebuild -C build
 awk -F":|'" '/version:/{print $3; exit}' ./meson.build > ~/version
